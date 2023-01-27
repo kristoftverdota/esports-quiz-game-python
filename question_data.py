f4aa3ecf9dd2f8ded1b71dba96d1d629d90b3e38
Kristof Tverdota <tverdota.kristof@gmail.com>
2023-01-27 16:58:08 +0100

    Create question_data.py
    50 questions from Open Trivia DB. You can insert your own questions here.

---
+++ question_data.py
@@ -0,0 +1,395 @@
+question_data = [
+    {
+      "category": "Entertainment: Video Games",
+      "type": "boolean",
+      "difficulty": "easy",
+      "question": "The song Megalovania by Toby Fox made its third appearance in the 2015 RPG Undertale.",
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", "difficulty": "easy",
+      "question": "The Indie Game Development Studio Cing, developers of Hotel Dusk and Last Window, went bankrupt on March 1st, 2010.",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "Undertale; is an RPG created by Toby Fox and released in 2015.",
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "hard",
+      "question": "In the game ;Melty Blood Actress Again Current Code;, you can enter Blood Heat mode in Half Moon style.",
+      "correct_answer": "False", 
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "The game ;Battlefield 1; takes place during World War I.", 
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "medium",
+      "question": "Metal Gear Solid V: The Phantom Pain runs on the Fox Engine.", 
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "In ;Super Mario 3D World;, the Double Cherry power-up originated from a developer accidentally making two characters controllable.",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "The ultimate phrase used by Pharah from Overwatch is: ;Justice rains from above!;",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "The PlayStation was originally a joint project between Sega and Sony that was a Sega Genesis with a disc drive.",
+     "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "Minecraft can be played with a virtual reality headset.", 
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "In Team Fortress 2, being disguised as a scout or medic results in a speed boost.",
+      "correct_answer": "False", 
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "medium",
+      "question": "TF2: The Medic will be credited for an assist if he heals a Spy that successfully saps a building.",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "medium",
+      "question": "TF2: The Heavy&#039;s voice actor, Gary Schwartz, voices the Demoman as well ",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "In the ;Half-Life; series, ;H.E.V; stands for ;Hazardous Evasiveness Vest;",
+      "correct_answer": "False", 
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "The main playable character of the 2015 RPG ;Undertale; is a monster.",
+      "correct_answer": "False", 
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "hard",
+      "question": "Danganronpa 2: Goodbye Despair featured all of the surviving students from the first game.",
+      "correct_answer": "False", 
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean",
+      "difficulty": "easy",
+      "question": "Rebecca Chambers does not appear in any Resident Evil except for the original Resident Evil and the Gamecube remake.",
+      "correct_answer": "False", 
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", "difficulty": "easy",
+      "question": "In the game ;Subnautica;, a ;Spadefish; will attack you.",
+      "correct_answer": "False", 
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "medium",
+      "question": "In the game ;Subnautica;, a ;Cave Crawler; will attack you.",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "Ana was added as a new hero for the game Overwatch on July 19th, 2016.", 
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "The end credits sequence in Grand Theft Auto 5 is over half an hour long.",
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "The names of Tom Nook&#039;s cousins in the Animal Crossing franchise are named ;Timmy; and ;Jimmy;.",
+      "correct_answer": "False", 
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "In Pok&eacute;mon Sun and Moon, a male Salandit can evolve to a Salazzle.",
+      "correct_answer": "False",
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "In Pok&eacute;mon, Bulbasaur is the only starter pokemon that is a Grass Poison type.",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "medium",
+      "question": "In Rocket League, you can play Basketball.", 
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "In ;Undertale;, the main character of the game is Sans.", 
+      "correct_answer": "False",
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "In RuneScape, one must complete the ;Dragon Slayer; quest before equipping Rune Platelegs.",
+      "correct_answer": "False", 
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "medium",
+      "question": "David Baszucki was a co-founder of ROBLOX Corporation.", 
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "Valve&#039;s ;Portal; and ;Half-Life; franchises exist within the same in-game universe.",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "medium",
+      "question": "In the video game ;Transistor;, ;Red; is the name of the main character.",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "Watch_Dogs 2 is a prequel.", 
+      "correct_answer": "False", 
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "In Heroes of the Storm, the Cursed Hollow map gimmick requires players to kill the undead to curse the enemy team.",
+      "correct_answer": "False", 
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean",
+      "difficulty": "medium",
+      "question": "Resident Evil 4 was originally meant to be a Nintendo GameCube exclusive.",
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", "difficulty": "easy",
+      "question": ";Metal Gear Solid 3: Snake Eater; was released in 2004.", 
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "The ghosts in ;Pac-Man; and ;Ms. Pac-Man; have completely different behavior.",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]},
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "medium",
+      "question": "In ;League of Legends;, there exists four different types of Dragon.",
+      "correct_answer": 
+      "False", "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "medium",
+      "question": ";Return to Castle Wolfenstein; was the only game of the Wolfenstein series where you play as William ;B.J.; Blazkowicz.",
+      "correct_answer": "False", "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "medium",
+      "question": "Hidden in the files for ;Mario Kart Arcade GP; is a picture of the Beslan school hostage crisis.",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean",
+      "difficulty": "easy",
+      "question": ";Sonic the Hedgehog 2; originally was going to have a time travel system.",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "hard",
+      "question": "In ;Minecraft;, gold tools are faster than diamond tools.", 
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "In Until Dawn, both characters Sam and Mike cannot be killed under any means until the final chapter of the game.",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "In the video game ;Splatoon;, the playable characters were originally going to be rabbits instead of squids.",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "Each piece in Tetris is called a tetris.", 
+      "correct_answer": "False",
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "The game ;Jetpack Joyride; was created by ;Redbrick Studios;.",
+      "correct_answer": "False", 
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "hard",
+      "question": "The Paradox Interactive game ;Stellaris; was released in 2016.",
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "hard",
+      "question": "In The Witcher 3, the Zoltan Chivay Gwent card can be found under the Hanged Man&#039;s Tree.",
+      "correct_answer": "True", 
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "In Resident Evil 4, the Chicago Typewriter has infinite ammo.", 
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "medium",
+      "question": "In the Resident Evil series, Leon S. Kennedy is a member of STARS.", 
+      "correct_answer": "False",
+      "incorrect_answers": ["True"]},
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": ";Resident Evil 7; is the first first-person Resident Evil game.",
+      "correct_answer": "False", 
+      "incorrect_answers": ["True"]
+    },
+    {
+      "category": "Entertainment: Video Games", 
+      "type": "boolean", 
+      "difficulty": "easy",
+      "question": "The shotgun appears in every numbered Resident Evil game.", 
+      "correct_answer": "True",
+      "incorrect_answers": ["False"]
+    }
+]
